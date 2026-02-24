a49131273121c70cac7c79b03a7746b6cd6fa7b5
Jennessa Sierra <2021153908@ub.edu.bz>
2026-02-24 02:06:31 -0600

    feat(db): added connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const appVersion = "1.0.0"
@@ -14,6 +18,9 @@ const appVersion = "1.0.0"
 type serverConfig struct {
 	port        int
 	environment string
+	db          struct {
+		dsn string
+	}
 }
 
 type applicationDependencies struct {
@@ -27,10 +34,22 @@ func main() {
 	flag.IntVar(&settings.port, "port", 4000, "Server port")
 	flag.StringVar(&settings.environment, "env", "development",
 		"Environment(development|staging|production)")
-	flag.Parse()
+	// read in the dsn
+	flag.StringVar(&settings.db.dsn, "db-dsn", "", "PostgreSQL DSN")
 
+	flag.Parse()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	// the call to openDB() sets up our connection pool
+	db, err := openDB(settings)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	// release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 	appInstance := &applicationDependencies{
 		config: settings,
 		logger: logger,
@@ -44,10 +63,33 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
-
 	logger.Info("starting server", "address", apiServer.Addr,
 		"environment", settings.environment)
-	err := apiServer.ListenAndServe()
+	err = apiServer.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(settings serverConfig) (*sql.DB, error) {
+	// open a connection pool
+	db, err := sql.Open("postgres", settings.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// set a context to ensure DB operations don't take too long
+	ctx, cancel := context.WithTimeout(context.Background(),
+		5*time.Second)
+	defer cancel()
+	// let's test if the connection pool was created
+	// we trying pinging it with a 5-second timeout
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	// return the connection pool (sql.DB)
+	return db, nil
+
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/jennxsierra/lab4-db-crud-implementation
 go 1.25.6
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.11.2
