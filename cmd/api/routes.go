23c49ead73c4bb2394d1679317d36d71a9c96fce
Jennessa Sierra <2021153908@ub.edu.bz>
2026-02-24 00:48:26 -0600

    chore(clean): removed hard-coded data from previous lab

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,11 +20,5 @@ func (a *applicationDependencies) routes() http.Handler {
 	// setup routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
 
-	// New APIs for appointments and patients (GET and POST)
-	router.HandlerFunc(http.MethodGet, "/v1/appointments", a.appointmentsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/appointments", a.appointmentsHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/patients", a.patientsHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/patients", a.patientsHandler)
-
 	return a.recoverPanic(router)
 }
